c502f4bb18cda06aa3e0b12e955102e7022a6530
RuslanKozin <ruslan3723@mail.ru>
2016-08-02 11:52:36 +0300

    Изменил заголовок страницы О нас в меню

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -36,7 +36,7 @@ AppAsset::register($this);
     ]);
     $menuItems = [
         ['label' => 'Home', 'url' => ['/site/index']],
-        ['label' => 'About', 'url' => ['/site/about']],
+        ['label' => 'О нас', 'url' => ['/site/about']],
         ['label' => 'Contact', 'url' => ['/site/contact']],
     ];
     if (Yii::$app->user->isGuest) {
