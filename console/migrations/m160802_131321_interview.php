e290aec37d90e8d51ffccac90f5625e0f006d985
RuslanKozin <ruslan3723@mail.ru>
2016-08-02 18:19:36 +0300

    Изменил файл миграции

--- console/migrations/m160802_131321_interview.php
+++ console/migrations/m160802_131321_interview.php
@@ -1,12 +1,41 @@
 <?php
 
+use yii\db\Schema;
 use yii\db\Migration;
 
 class m160802_131321_interview extends Migration
 {
+    /**
+     * Поддерживаемые абстрактные типы данных для описания колонок.
+     */
+    const TYPE_PK = 'pk';
+    const TYPE_BIGPK = 'bigpk';
+    const TYPE_STRING = 'string';
+    const TYPE_TEXT = 'text';
+    const TYPE_SMALLINT = 'smallint';
+    const TYPE_INTEGER = 'integer';
+    const TYPE_BIGINT = 'bigint';
+    const TYPE_FLOAT = 'float';
+    const TYPE_DOUBLE = 'double';
+    const TYPE_DECIMAL = 'decimal';
+    const TYPE_DATETIME = 'datetime';
+    const TYPE_TIMESTAMP = 'timestamp';
+    const TYPE_TIME = 'time';
+    const TYPE_DATE = 'date';
+    const TYPE_BINARY = 'binary';
+    const TYPE_BOOLEAN = 'boolean';
+    const TYPE_MONEY = 'money';
+
     public function up()
     {
-
+        $this->createTable('{{%interview}}', [
+            'id' => Schema::TYPE_PK,
+            'name' => Schema::TYPE_STRING . ' NOT NULL',
+            'sex' => Schema::TYPE_BOOLEAN . ' NOT NULL',
+            'planets' => Schema::TYPE_STRING . ' NOT NULL',
+            'astronauts' => Schema::TYPE_STRING. ' NOT NULL',
+            'planet' => Schema::TYPE_INTEGER . ' NOT NULL',
+        ]);
     }
 
     public function down()
