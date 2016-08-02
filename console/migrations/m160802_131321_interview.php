030a8bc3b5fba35bcd47726655cb3d050c8dc4dc
RuslanKozin <ruslan3723@mail.ru>
2016-08-02 16:18:16 +0300

    Добавил файл миграции для таблицы, которая будет хранить данные из формы "Опрос"

---
+++ console/migrations/m160802_131321_interview.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+class m160802_131321_interview extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m160802_131321_interview cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
