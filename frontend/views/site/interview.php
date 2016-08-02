a47e24889a21a2d00d06c99c3f6031f95ad7b7a8
RuslanKozin <ruslan3723@mail.ru>
2016-08-02 16:50:37 +0300

    Создал файл\вид для формы опроса

---
+++ frontend/views/site/interview.php
@@ -0,0 +1,22 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model frontend\models\Interview */
+/* @var $form ActiveForm */
+?>
+<div class="site-interview">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+        <?= $form->field($model, 'version') ?>
+        <?= $form->field($model, 'apply_time') ?>
+    
+        <div class="form-group">
+            <?= Html::submitButton('Submit', ['class' => 'btn btn-primary']) ?>
+        </div>
+    <?php ActiveForm::end(); ?>
+
+</div><!-- site-interview -->
