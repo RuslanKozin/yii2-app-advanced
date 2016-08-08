a813ad3fbf189a10deb6529cc12d32384d2abd0a
RuslanKozin <ruslan3723@mail.ru>
2016-08-08 15:45:56 +0300

    Добавил файл миграции

---
+++ console/migrations/m160808_124339_create_asto_tables.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+class m160808_124339_create_asto_tables extends Migration
+{
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m160808_124339_create_asto_tables cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
